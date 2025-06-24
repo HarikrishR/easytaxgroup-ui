5794d6249a55b579cbae301b47861b1e287c1182
Hari Krishnan R <harikrish12498@gmail.com>
2025-06-24 13:08:04 +0530

    updated title logo

--- src/authContext.tsx
+++ src/authContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, ReactNode, useEffect } from 'react';
-import { toast } from 'react-toastify';
+// import { toast } from 'react-toastify';
 
 interface AuthContextType {
   user: string | null;
